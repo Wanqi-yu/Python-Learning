f0648c5557ccbf5ef98d456f83bf0b4b4de3c55d
Wanqi-Yu <2395512467@qq.com>
2025-06-23 20:20:13 +0200

    Add files via upload

---
+++ Day_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2025/6/23 20:18
+# @Author : wanqi.yu
+# @File : Day_01.py
+# @Software: PyCharm
+'''
+This is a learning Python the Program
+'''
+print("Hello World")
+
+if __name__ == "__main__":
+    run_code = 0
